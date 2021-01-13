bd91db7f90a15d5d5d594e63a6c30da7ab34f329
zhouzhonglou <z.zhou@herin.com>
2021-01-13 16:22:01 +0800

    添加vue.config配置文件

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-01-13 16:21:29
+ * @LastEditTime: 2021-01-13 16:21:39
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /git-test/vue.config.js
+ */
+module.exports = {
+    publicPath: './'
+}
